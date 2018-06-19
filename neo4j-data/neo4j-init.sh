11d0abccd57fc9c3504833c529dd5368bd25aeb0
Radim Peša <2780@mail.muni.cz>
2018-06-19 13:16:30 +0200

    importing data

--- neo4j-data/neo4j-init.sh
+++ neo4j-data/neo4j-init.sh
@@ -4,6 +4,7 @@ if [ ! -f /data/neo4j-import-done.flag ]; then
     echo "Import database."
     mkdir -p /data/databases/graph.db
     tar -zxvf /neo4j-data/graph.tgz -C /data/databases/graph.db
+#    tar -zxvf /neo4j-data/pdx.tgz -C /data/databases/graph.db
     touch /data/neo4j-import-done.flag
     chmod 0777 /data/databases/graph.db/*
 else
